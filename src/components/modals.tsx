226f318c31ad7a1949775e973d7b32007892692f
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-12-27 13:14:11 +0530

    we can prevent hyderation error with mounting

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,7 +1,17 @@
 import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
+import { useEffect, useState } from "react";
+import { mutation } from "../../convex/_generated/server";
 
 const Modals = () => {
-    
+    // to prevent hyderation error 
+    const [mouted, setMounted] = useState(false);
+    // useEffect only work with "use client"   
+    useEffect(() => {
+       setMounted(true)   
+    }, [])
+    if (!mouted) { 
+        return null
+    }
     return (
         <>
             <CreateWorkspaceModal/>
