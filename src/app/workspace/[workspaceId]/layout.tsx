77353adebe3474e87155c2f7852c38094bc76d9f
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-12-31 16:16:01 +0530

    display the workspace with dropdown option invite people if the role is admin

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,22 +1,41 @@
 "use client";
 import Sidebar from "./sidebar";
 import Toolbar from "./toolbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import WorkspaceSidebar from "./workspace-sidebar";
 
-interface WorkspaceIdLayoutProps{
-    children:React.ReactNode
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode;
 }
-const WorkspaceIdLayout = ({children}:WorkspaceIdLayoutProps) => {
-    return (  
-        <div className="h-full">
-            {/* we have substract height of toolbar below 40px */}
-            <Toolbar />
-            {/*  */}
-            <div className="flex h-[calc(100vh-40px)]">
-            <Sidebar/>
-            {children}
-            </div>
-        </div>
-    );
-}
- 
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return (
+    <div className="h-full">
+      {/* we have substract height of toolbar below 40px */}
+      <Toolbar />
+      {/*  */}
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+              <ResizablePanelGroup
+                  direction="horizontal"
+                autoSaveId="layout-"
+              >
+                  <ResizablePanel
+                      defaultSize={20}
+                      minSize={11}
+                      className="bg-[#5E2C5F]"
+                  >
+                  <WorkspaceSidebar/>
+                  </ResizablePanel>
+                  <ResizableHandle withHandle/>
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </div>
+  );
+};
+
 export default WorkspaceIdLayout;
---
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,37 @@
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { AlertTriangle, Loader } from "lucide-react";
+import WorkspaceHeader from "../workspace-header";
+
+const WorkspaceSidebar = () => {
+  const workspaceId = useWorkspaceId();
+  const {data:member,isLoading:memberLoading} = useCurrentMember({workspaceId});
+  const {data:workspace,isLoading:workspaceLoading} = useGetWorkspace({id:workspaceId});
+    if (workspaceLoading || memberLoading) {
+        return (
+            <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+              <Loader className="size-5 animate-spin text-white"/>  
+           </div>
+       )
+    }
+     if (!member || !workspace) {
+       return (
+         <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+               <AlertTriangle className="size-5  text-white" />
+               <p className="text-white text-sm">
+                   Workspace not found!
+               </p>
+         </div>
+       );
+     }
+ 
+    return (
+      <div className="flex flex-col bg-[#5E2C5F]">
+        <WorkspaceHeader workspace={workspace} isAdmin={ member.role === "admin"} />
+      </div>
+    );
+};
+ 
+export default WorkspaceSidebar;
