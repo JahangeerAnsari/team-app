742375f773d5b517528cc608d8b32de033bd609a
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-01-28 22:40:16 +0530

    image can be rended on in message input box with remove icon

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -10,10 +10,11 @@ import {
 import { PiTextAa } from "react-icons/pi";
 import { MdSend } from "react-icons/md";
 import { Button } from "./ui/button";
-import { ImageIcon, Key, Keyboard, Smile } from "lucide-react";
+import { ImageIcon, Key, Keyboard, Smile, Vault, XIcon } from "lucide-react";
 import Hint from "./hint";
 import { cn } from "@/lib/utils";
 import { EmojiPopover } from "./emoji-popover";
+import Image from "next/image";
 type EditorValue = {
   image: File | null;
   body: string;
@@ -136,13 +137,45 @@ const Editor = ({
   };
   return (
     <div className="flex flex-col">
-      <input type="file" className="hidden" accept="image/*" ref={imageElementRef} onChange={(e) =>setImage(e.target.files![0])}/>
+      <input
+        type="file"
+        className="hidden"
+        accept="image/*"
+        ref={imageElementRef}
+        onChange={(e) => setImage(e.target.files![0])}
+      />
       <div
         className="flex flex-col border border-slate-200 rounded-md
             overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white
             "
       >
         <div ref={containerRef} className="h-full ql-custom" />
+        {/* now we can display image */}
+        {!!image && (
+          <div className="p-2">
+            <div className="relative size-[62px] flex items-center justify-center group/image">
+              <Hint label="Remove Image">
+                <button
+                  onClick={() => {
+                    setImage(null);
+                    imageElementRef.current!.value = "";
+                  }}
+                  className="hidden group-hover/image:flex 
+                  rounded-full bg-black-70 hover:bg-black absolute -top-2.5 -right-2.5
+                   text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                >
+                  <XIcon className="size-3.5" />
+                </button>
+              </Hint>
+              <Image
+                src={URL.createObjectURL(image)}
+                alt="Uploaded"
+                fill
+                className="rounded-xl overflow-hidden border object-cover"
+              />
+            </div>
+          </div>
+        )}
         <div className="flex px-2 pb-2 z-[5]">
           <Hint
             label={isToolbarVisible ? "Hide formatting" : "Show formatting"}
